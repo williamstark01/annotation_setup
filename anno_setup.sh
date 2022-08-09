c7f8ad73ca85529b261bc35863e2153b0c9a1c62
William Stark <william@ebi.ac.uk>
2022-08-09 20:14:58 +0100

    more setting up

--- anno_setup.sh
+++ anno_setup.sh
@@ -132,3 +132,107 @@ annotation_enscode_directory="${ANNOTATION_CODE_DIRECTORY}/enscode"
 
 mkdir --parents --verbose "$annotation_enscode_directory"
 ################################################################################
+
+
+# populate the annotation enscode directory
+################################################################################
+cd "$enscode_directory"
+
+repository="ensembl-analysis"
+cd "$repository"
+git worktree prune
+git branch -D "$ANNOTATION_NAME" &>/dev/null || true
+git branch "$ANNOTATION_NAME" "origin/experimental/gbiab"
+git worktree add "${annotation_enscode_directory}/${repository}" "$ANNOTATION_NAME"
+cd "$enscode_directory"
+
+repository="ensembl-genes"
+cd "$repository"
+git worktree prune
+git branch -D "$ANNOTATION_NAME" &>/dev/null || true
+git branch "$ANNOTATION_NAME" "main"
+git worktree add "${annotation_enscode_directory}/${repository}" "$ANNOTATION_NAME"
+cd "$enscode_directory"
+
+repositories=(
+    "ensembl"
+    "ensembl-io"
+    "ensembl-production"
+    "ensembl-hive"
+    "ensembl-compara"
+    "ensembl-killlist"
+    "ensembl-taxonomy"
+    "ensembl-variation"
+    "ensembl-datacheck"
+    "ensembl-metadata"
+    "ensembl-orm"
+)
+
+for repository in "${repositories[@]}"; do
+    ln --symbolic --verbose "${enscode_directory}/${repository}" "${annotation_enscode_directory}/${repository}"
+done
+################################################################################
+
+
+# create the annotation log directory
+################################################################################
+cd "$ANNOTATION_CODE_DIRECTORY"
+
+# create directory for storing the annotation config files
+ANNOTATION_LOG_DIRECTORY="${ANNOTATION_CODE_DIRECTORY}/annotation"
+mkdir --verbose "$ANNOTATION_LOG_DIRECTORY"
+
+cd "$ANNOTATION_LOG_DIRECTORY"
+###############################################################################
+
+
+# generate some values, create parent data directories
+################################################################################
+annotations_data_root="/hps/nobackup/flicek/ensembl/genebuild/${USER}/annotations"
+
+ANNOTATION_DATA_DIRECTORY="${annotations_data_root}/${ANNOTATION_NAME}"
+
+#JOB_QUEUE="production"
+JOB_QUEUE="short"
+
+# create parent data directories
+bsub -q $JOB_QUEUE -Is mkdir --parents --verbose "$ANNOTATION_DATA_DIRECTORY"
+# add write permission to file group
+bsub -q $JOB_QUEUE -Is chmod --verbose g+w "$ANNOTATION_DATA_DIRECTORY"
+################################################################################
+
+
+# copy and update EnsemblAnno_conf.pm
+# existing file template:
+# https://github.com/Ensembl/ensembl-analysis/blob/experimental/gbiab/modules/Bio/EnsEMBL/Analysis/Hive/Config/EnsemblAnno_conf.pm
+################################################################################
+pipeline_config_path="${ANNOTATION_LOG_DIRECTORY}/EnsemblAnno_conf.pm"
+
+pipeline_config_template_path="${annotation_enscode_directory}/ensembl-analysis/modules/Bio/EnsEMBL/Analysis/Hive/Config/EnsemblAnno_conf.pm"
+
+cp --preserve --verbose "$pipeline_config_template_path" "$pipeline_config_path"
+
+# characters to escape in sed substitutions: ^.[]/\$*
+# https://unix.stackexchange.com/questions/32907/what-characters-do-i-need-to-escape-when-using-sed-in-a-sh-script/33005#33005
+# https://en.wikipedia.org/wiki/Regular_expression#POSIX_basic_and_extended
+
+sed --in-place -e "s|'base_output_dir'              => '',|'base_output_dir'              => '$ANNOTATION_DATA_DIRECTORY',|g" "$pipeline_config_path"
+
+scientific_name_underscores_lower_case="${scientific_name_underscores,,}"
+assembly_accession_underscores="${ASSEMBLY_ACCESSION//./_}"
+assembly_accession_underscores_lower_case="${assembly_accession_underscores,,}"
+sed --in-place -e "s/'production_name'              => '',/'production_name'              => '$scientific_name_underscores_lower_case-$assembly_accession_underscores_lower_case',/g" "$pipeline_config_path"
+
+sed --in-place -e "s/-input_ids         => \[\],/-input_ids         => \[{'assembly_accession' => '$ASSEMBLY_ACCESSION'}\],/g" "$pipeline_config_path"
+################################################################################
+
+
+# update ProcessGCA.pm
+# https://github.com/Ensembl/ensembl-analysis/blob/experimental/gbiab/modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/ProcessGCA.pm
+################################################################################
+# ensembl-analysis/modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/ProcessGCA.pm
+ProcessGCA_path="${annotation_enscode_directory}/ensembl-analysis/modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/ProcessGCA.pm"
+
+sed --in-place -e "s/my \$current_genebuild = \$self->param('current_genebuild');/#my \$current_genebuild = \$self->param('current_genebuild');/g" "$ProcessGCA_path"
+sed --in-place -e "s/#my \$current_genebuild  = 0;/my \$current_genebuild  = 1;/g" "$ProcessGCA_path"
+################################################################################
